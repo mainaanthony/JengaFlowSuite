a63cd45d7ac599f85f48ba2bf219619eb2b39dd4
mainaanthony <mainaanthonymwai@gmail.com>
2025-09-23 11:44:30 +0300

    feat: add the shared folder to use for components, dashboard componen initialize, main ts file update to inject routes, define initial app routes structure

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,13 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { keycloakInterceptor } from './app/keycloak.interceptor';
 import { APP_INITIALIZER } from '@angular/core';
+import { APP_ROUTES} from '../src/app/app.routes'
 import { initKeycloak } from './app/keycloak.init';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideRouter(APP_ROUTES, withComponentInputBinding()),
     provideHttpClient(withInterceptors([keycloakInterceptor])),
     { provide: APP_INITIALIZER, useFactory: initKeycloak, multi: true },
   ]
